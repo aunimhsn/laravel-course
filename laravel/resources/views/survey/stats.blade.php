1f3944a136949204dedcb594851465df8dc2e95b
Aunim HASSAN <aunim.hassan@gmail.com>
2021-03-14 16:31:00 +0100

    ui: better progress bar

--- laravel/resources/views/survey/stats.blade.php
+++ laravel/resources/views/survey/stats.blade.php
@@ -15,19 +15,27 @@
         <div class="flex flex-col items-start">
             <div class="my-2 w-96">
                 <h2>Super Street Fighter IV ({{ $results['ssf4'] }}%)</h2>
-                <div class="h-6 my-1 bg-blue-500 rounded-sm" style="width: {{ $results['ssf4'] }}%"></div>
+                <div class="w-full bg-blue-200">
+                    <div class="h-6 my-1 bg-blue-500 rounded-sm" style="width: {{ $results['ssf4'] }}%"></div>
+                </div>
             </div>
             <div class="my-2 w-96">
                 <h2>Super Smash Bros Melee ({{ $results['ssbm'] }}%)</h2>
-                <div class="h-6 my-1 bg-blue-500 rounded-sm" style="width: {{ $results['ssbm'] }}%"></div>
+                <div class="w-full bg-blue-200">
+                    <div class="h-6 my-1 bg-blue-500 rounded-sm" style="width: {{ $results['ssbm'] }}%"></div>
+                </div>
             </div>
             <div class="my-2 w-96">
                 <h2>Soulcalibur IV ({{ $results['s4'] }}%)</h2>
-                <div class="h-6 my-1 bg-blue-500 rounded-sm" style="width: {{ $results['s4'] }}%"></div>
+                <div class="w-full bg-blue-200">
+                    <div class="h-6 my-1 bg-blue-500 rounded-sm" style="width: {{ $results['s4'] }}%"></div>
+                </div>
             </div>
             <div class="my-2 w-96">
                 <h2>Mortal Kombat ({{ $results['mku'] }}%)</h2>
-                <div class="h-6 bg-blue-500 rounded-sm" style="width: {{ $results['mku'] }}%"></div>
+                <div class="relative w-full bg-blue-200">
+                    <div class="h-6 bg-blue-500 rounded-sm" style="width: {{ $results['mku'] }}%"></div>
+                </div>
             </div>
 
             <a href="{{ route('survey.index') }}" class="hover:bg-blue-600 mt-4 px-4 py-2 bg-blue-800 text-white">Back to vote</a>
